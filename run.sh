6117740a1acc700988176cb798c1fcde7222b3de
Brad Caron <bacaron@iu.edu>
2020-01-17 21:31:30 -0500

    Reason: initial commit

---
+++ run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+icvf=`jq -r '.icvf' config.json`
+od=`jq -r '.od' config.json`
+isovf=`jq -r '.isovf' config.json`
+ad=`jq -r '.ad' config.json`
+fa=`jq -r '.fa' config.json`
+md=`jq -r '.md' config.json`
+rd=`jq -r '.rd' config.json`
+mask=`jq -r '.mask' config.json`
+roi=`jq -r '.roi' config.json`
+parc=`jq -r '.parc' config.json`
+
+if [ ! -f ${icvf} ]; then
+	metric="ad fa md rd"
+	echo ${metric}
+elif [ -f ${icvf} ] && [ -f ${fa} ]; then
+	metric="icvf od isovf ad fa md rd"
+	echo ${metric}
+else
+	metric="icvf od isovf"
+	echo ${metric}
+fi
+
+# if mask
+#if [ -f ${mask} ]; then
+	for MET in ${metric}
+	do
+		if [ ! -f ${MET}_masked.nii.gz ]; then
+			fslmaths ${!MET} -mul ${mask} ${MET}_masked.nii.gz
+		fi
+		fslstats ${MET}_masked.nii.gz -M > ${MET}.txt
+	done
+#else
+#fi
+
+printf '%s\n' ${metric} | paste -sd ',' >> output.csv
+printf '%s\n' `cat *.txt` | paste -sd ',' >> output.csv
+
+# binarize roi
+#for ROI in dwi_*.nii.gz
+#do
+#	fslmaths ${ROI} -bin bin_${ROI}
+#	for MET in ${metric}
+#	do
+#		fslmaths ${!MET} -mul bin_${ROI} ${MET}_${ROI}
+#	done
+#done
